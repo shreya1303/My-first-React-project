0d7306fa100f42d542baf9272f61d9ad9a9f097d
shreya1303 <135065442+shreya1303@users.noreply.github.com>
2023-09-14 19:39:21 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Body from './components/body';
+import Navigation from './components/Navigation';
+
+const App = () => {
+    return <div>
+           <Navigation />
+           <Body />
+    </div>
+    
+}; 
+
+export default App;
---
+++ Body.jsx
@@ -0,0 +1,25 @@
+const Body = () => {
+    return (
+        <main className="Body">
+            <div className="Body-content">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p >YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+            <div className="btn">
+            <button>Shop Now</button>
+            <button className="second">Category</button>
+            </div>
+            <div className="availabe">
+                <p>Also Available On</p>
+                <div className="icons">
+                    <img src="/images/amazon.png" alt="amazon-logo" />
+                    <img src="/images/flipkart.png" alt="flipkart-logo" />
+                </div>
+            </div>
+            </div>
+            <div className="Body-image">
+            <img src="/images/shoe_image.png" alt="shoe-image" />
+            </div>
+        </main>
+    );
+}
+export default Body;
